0ebdcc1139f0290916e530bf076415454490e99e
Jared <126293208+indohippi@users.noreply.github.com>
2024-05-22 23:02:33 -0500

    Update index.web.js to resolve import issues with react-refresh

--- src/index.web.js
+++ src/index.web.js
@@ -1,11 +1,12 @@
 import { AppRegistry } from 'react-native';
 import App from './App';
 import appJson from './App.json';
-import 'react-native-gesture-handler';
+import 'react-refresh/runtime';  // Ensure this line points to src/react-refresh/runtime.js
 
 AppRegistry.registerComponent(appJson.name, () => App);
 AppRegistry.runApplication(appJson.name, {
   initialProps: {},
   rootTag: document.getElementById('app-root'),
 });
+
 
