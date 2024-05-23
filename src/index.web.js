7bd91ea345a7a803afe74996ef96f121d832ebab
Jared <126293208+indohippi@users.noreply.github.com>
2024-05-22 22:54:14 -0500

    Update index.web.js to resolve import issues

--- src/index.web.js
+++ src/index.web.js
@@ -1,6 +1,7 @@
 import { AppRegistry } from 'react-native';
 import App from './App';
 import appJson from './App.json';
+import 'react-native-gesture-handler';
 
 AppRegistry.registerComponent(appJson.name, () => App);
 AppRegistry.runApplication(appJson.name, {
