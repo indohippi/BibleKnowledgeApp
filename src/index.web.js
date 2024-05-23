b8f1d6165777ff11854c7ebb0051060f41eedc59
Jared <126293208+indohippi@users.noreply.github.com>
2024-05-22 23:04:30 -0500

    Remove react-refresh import from index.web.js

--- src/index.web.js
+++ src/index.web.js
@@ -1,12 +1,12 @@
 import { AppRegistry } from 'react-native';
 import App from './App';
 import appJson from './App.json';
-import 'react-refresh/runtime';  // Ensure this line points to src/react-refresh/runtime.js
 
 AppRegistry.registerComponent(appJson.name, () => App);
 AppRegistry.runApplication(appJson.name, {
   initialProps: {},
   rootTag: document.getElementById('app-root'),
 });
+
 
 
