0610577ca18906cc9b26ba5d45af90836c1ed2d5
Jared <126293208+indohippi@users.noreply.github.com>
2024-05-22 21:55:25 -0500

    Add craco.config.js to override webpack configuration

---
+++ craco.config.js
@@ -0,0 +1,15 @@
+const path = require('path');
+
+module.exports = {
+  webpack: {
+    alias: {
+      'react-native$': 'react-native-web',
+    },
+    configure: (webpackConfig, { env, paths }) => {
+      // Override the default Webpack configuration here
+      // Move react-refresh to a symlink inside src directory
+      webpackConfig.resolve.alias['react-refresh/runtime'] = path.resolve(__dirname, 'src', 'react-refresh', 'runtime.js');
+      return webpackConfig;
+    },
+  },
+};
