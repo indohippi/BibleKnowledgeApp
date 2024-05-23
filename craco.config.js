eb9a5e42961e2e8e325c8efdc81b106ee71fc9e8
Jared <126293208+indohippi@users.noreply.github.com>
2024-05-22 22:53:05 -0500

    Adjust craco.config.js for correct react-refresh alias

--- craco.config.js
+++ craco.config.js
@@ -4,13 +4,9 @@ module.exports = {
   webpack: {
     alias: {
       'react-native$': 'react-native-web',
-    },
-    configure: (webpackConfig, { env, paths }) => {
-      // Override the default Webpack configuration here
-      // Move react-refresh to a symlink inside src directory
-      webpackConfig.resolve.alias['react-refresh/runtime'] = path.resolve(__dirname, 'src/react-refresh/runtime.js');
-      return webpackConfig;
-    },
-  },
+      'react-refresh/runtime': path.resolve(__dirname, 'src/react-refresh/runtime.js')
+    }
+  }
 };
+
 
