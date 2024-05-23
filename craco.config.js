83e663d99f6ac242cbc7af2f2e6db120df887f67
Jared <126293208+indohippi@users.noreply.github.com>
2024-05-22 22:52:02 -0500

    Update craco.config.js to handle react-refresh alias

--- craco.config.js
+++ craco.config.js
@@ -8,8 +8,9 @@ module.exports = {
     configure: (webpackConfig, { env, paths }) => {
       // Override the default Webpack configuration here
       // Move react-refresh to a symlink inside src directory
-      webpackConfig.resolve.alias['react-refresh/runtime'] = path.resolve(__dirname, 'src', 'react-refresh', 'runtime.js');
+      webpackConfig.resolve.alias['react-refresh/runtime'] = path.resolve(__dirname, 'src/react-refresh/runtime.js');
       return webpackConfig;
     },
   },
 };
+
