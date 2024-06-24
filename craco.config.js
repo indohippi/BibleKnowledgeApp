4a4d96b7d5c01573acb7348e74728ef248c65c0d
Jared <126293208+indohippi@users.noreply.github.com>
2024-06-23 22:04:52 -0500

    Describe your changes

--- craco.config.js
+++ craco.config.js
@@ -3,9 +3,7 @@ const path = require('path');
 module.exports = {
   webpack: {
     alias: {
-      'react-native$': 'react-native-web',
-      'react-refresh/runtime': path.resolve(__dirname, 'src/react-refresh/runtime.js')
+      'react-native$': 'react-native-web'
     }
   }
 };
-
