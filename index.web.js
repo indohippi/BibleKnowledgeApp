1c317836096fd6e2d2840b4ac1999d8e5dcd5611
Jared <126293208+indohippi@users.noreply.github.com>
2024-07-10 17:25:34 -0500

    Update index.web.js to include react-refresh setup

--- index.web.js
+++ index.web.js
@@ -1,3 +1,5 @@
+import './setupReactRefresh'; // Ensure this is at the top if react-refresh needs to be initialized first
+
 import { AppRegistry } from 'react-native';
 import App from './App';
 import { name as appName } from './app.json';
